3177e857117cda413d3cc83374eb6516addc7ffa
abdullah-al-monir <abdullahalmonir364@gmail.com>
2025-06-15 20:37:58 +0600

    Changes some code

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-center font-bold text-purple-500 text-xl underline ">React with Vite</h1>
-      <h2 className="text-center font-bold">Tailwind CSS</h2>
+      <h2 className="text-red-500 text-4xl text-center">Welcome</h2>
     </>
   )
 }
