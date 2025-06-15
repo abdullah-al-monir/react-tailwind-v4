46c0beb378f469156323b7c67c03fc8142ade1a5
abdullah-al-monir <abdullahalmonir364@gmail.com>
2025-06-15 19:48:16 +0600

    Tailwind CSS Latest Version with react js

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-center font-bold text-purple-500 text-xl underline ">React with Vite</h1>
+      <h2 className="text-center font-bold">Tailwind CSS</h2>
+    </>
+  )
+}
+
+export default App
